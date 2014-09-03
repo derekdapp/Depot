ff7fa51f69f42ef0f197818dbee5cc26d76af565
Derek Dapp <derekdapp@dereks-air.domain.actdsltmp>
2014-09-03 13:45:22 -0600

    Finished adding tests

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -40,7 +40,7 @@ class CartsControllerTest < ActionController::TestCase
   end
 
   test "should destroy cart" do
-    assert_difference('Cart.count', -1) 
+    assert_difference('Cart.count', -1) do
       session[:cart_id] = @cart.id
       delete :destroy, id: @cart
     end
